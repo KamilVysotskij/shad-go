88b7c4e819cd0b255d9ea3c1c9c39bb3aa047365
Kamil <kamil@MacBook-Kamil.local>
2025-08-02 02:11:57 +0500

    done

--- urlfetch/main.go
+++ urlfetch/main.go
@@ -2,6 +2,37 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
 
+func checkErr(err error) {
+	if err != nil {
+		msg := "fetch:"
+		log.SetPrefix("")
+		log.SetFlags(0)
+		log.Fatalf("%s %v", msg, err)
+	}
+}
+
+func checkUrl(url string) {
+	response, err := http.Get(url)
+	checkErr(err)
+	body, err := io.ReadAll(response.Body)
+	response.Body.Close()
+	checkErr(err)
+	fmt.Printf("%s", body)
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		os.Exit(1)
+	}
+	for _, url := range os.Args[1:] {
+		checkUrl(url)
+	}
 }
