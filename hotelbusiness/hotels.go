e030ac42437bb2a06a8c7df18fda14b9cc8140d4
Kamil <kamil.vysotskij@yandex.ru>
2025-08-07 01:20:10 +0500

    done

--- hotelbusiness/hotels.go
+++ hotelbusiness/hotels.go
@@ -2,6 +2,8 @@
 
 package hotelbusiness
 
+import "sort"
+
 type Guest struct {
 	CheckInDate  int
 	CheckOutDate int
@@ -13,5 +15,33 @@ type Load struct {
 }
 
 func ComputeLoad(guests []Guest) []Load {
-	return []Load{}
+	// Собираем все дни с изменениями
+	var result []Load
+	dateChangeMap := make(map[int]int)
+	for _, guest := range guests {
+		dateChangeMap[guest.CheckInDate] += 1
+		dateChangeMap[guest.CheckOutDate] -= 1
+	}
+	// Собираем все дни
+	var dates []int
+	for date := range dateChangeMap {
+		dates = append(dates, date)
+	}
+	sort.Ints(dates)
+	// Проходим по отсортированным дням и фиксируем кол-во гостей
+	currentGuests := 0
+
+	// Проходим по отсортированным датам и вычисляем текущее количество гостей
+	for _, date := range dates {
+		// Изменяем текущее количество гостей
+		currentGuests += dateChangeMap[date]
+		// Добавляем запись в результат, если количество гостей изменилось
+		if len(result) == 0 || result[len(result)-1].GuestCount != currentGuests {
+			result = append(result, Load{
+				StartDate:  date,
+				GuestCount: currentGuests,
+			})
+		}
+	}
+	return result
 }
