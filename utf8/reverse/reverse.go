af3edd5daa64d6a14fcf5cbefa45baaa515f6cd9
Kamil <kamil.vysotskij@yandex.ru>
2025-10-17 01:30:52 +0500

    reverse

--- utf8/reverse/reverse.go
+++ utf8/reverse/reverse.go
@@ -2,6 +2,24 @@
 
 package reverse
 
+import (
+	"strings"
+)
+
 func Reverse(input string) string {
-	return ""
+	var sb strings.Builder
+	m := make(map[int]string)
+	i := 1
+	for _, r := range input {
+		m[i] = string(r)
+		i++
+	}
+	var str_list []string
+	for j := i; j > 0; j-- {
+		str_list = append(str_list, m[j])
+	}
+	for _, str := range str_list {
+		sb.WriteString(str)
+	}
+	return sb.String()
 }
